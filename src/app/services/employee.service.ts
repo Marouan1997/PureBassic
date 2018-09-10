3765adc932590daf5ad822efa140123161f59094
Hamza Hamidi <hamidihamzacg@gmail.com>
2018-09-11 01:27:12 +0200

    Fix

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,32 +1,26 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase , AngularFireList , AngularFireObject} from 'angularfire2/database';
-import { Observable }from 'rxjs';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 import { Employee } from '../Employee';
 
 @Injectable({
   providedIn: 'root'
- 
+
 })
 
 export class EmployeeService {
-  
-employees : AngularFireList <any[]>;
-Employee: AngularFireObject <any>;
+
+  employees: AngularFireList<Employee>;
   constructor(public af: AngularFireDatabase) {
-    this.employees = this.af.list('/employees/Employees') as AngularFireList<Employee[]>
-   }
+    this.employees = this.af.list('/employees/Employees');
+  }
 
-   getEmployees():  Observable<any>
-   {
-
+  getEmployees(): Observable<Employee[]> {
     return this.employees.valueChanges();
-   }
+  }
 
-   addEmployee( employee : Employee )
-   {
-     
-   this.employees.push(employee);
-    
-   }
+  addEmployee(employee: Employee): void {
+    this.employees.push(employee);
+  }
 }
 
